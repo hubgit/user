d88095299a8e52fab31cd1619772e4db97a9da72
alf <sites@hubmed.org>
2009-01-07 18:02:53 +0000

    misc

--- register.php
+++ register.php
@@ -10,7 +10,10 @@ if ($name && array_key_exists('password', $_POST))
   user_register($name, $_POST['password']);
   
 if ($_SESSION['uid'])
-  goto('edit.php', sprintf('<p>Welcome <b>%s</b></p><p>Edit your profile here if you like.</p>', filter_var($name, FILTER_SANITIZE_SPECIAL_CHARS)));
+  goto('edit.php', sprintf(
+    '<p>Welcome <b>%s</b></p>
+    <p>Edit your profile here if you like (an email address would be nice).</p>
+    <p>Otherwise, <a href="%s">continue to the front page</a>.</p>', filter_var($name, FILTER_SANITIZE_SPECIAL_CHARS), $config['site']['root']));
 
 $title = 'Register a new account';
 include 'includes/output.inc.php'; 
